dd806282d56870b09e8b2fb2adc9d889207cadf1
605s <ploki7335@naver.com>
2023-07-05 13:26:15 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,36 +1,16 @@
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 function Customer(pp) {
     return(
-        <div>
-            <CustomerProfile
-                id={pp.id}
-                name={pp.name}
-            />
-            <CustomerInfo
-                birthday={pp.birthday}
-                gender={pp.gender}
-                job={pp.job}
-            />
-        </div>
+        <TableRow>
+            <TableCell>{pp.id}</TableCell>
+            <TableCell>{pp.name}</TableCell>
+            <TableCell>{pp.birthday}</TableCell>
+            <TableCell>{pp.gender}</TableCell>
+            <TableCell>{pp.job}</TableCell>
+        </TableRow>
     );
-}
-
-function CustomerProfile(pp) {
-    return(
-        <div>
-            <h2>{pp.name}({pp.id})</h2>
-        </div>
-    )
-}
-
-function CustomerInfo(pp) {
-    return(
-        <div>
-            <p>{pp.birthday}</p>
-            <p>{pp.gender}</p>
-            <p>{pp.job}</p>
-        </div>
-    )
 }
 
 export default Customer
