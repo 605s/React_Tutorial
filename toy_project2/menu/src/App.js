da27bb13ebbdba6c6649d561caea00745ffbe237
605s <ploki7335@naver.com>
2023-07-05 18:34:08 +0900

    Add Function

--- toy_project2/menu/src/App.js
+++ toy_project2/menu/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Count from "./pages/Count"
+import Input from "./pages/Input"
 import React from "react";
 import {Route, Routes, Link} from 'react-router-dom'
 
@@ -7,12 +9,13 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <Link to ='/'>Home</Link> / 
-        <Link to ='/about'>About</Link>
+        <Link to ='/'>Home</Link> / <Link to ='/about'>About</Link> / <Link to ='/count'>Count</Link> / <Link to ='/input'>Input</Link>
       </nav>
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/count' element={<Count />}></Route>
+        <Route path='/input' element={<Input />}></Route>
       </Routes>
     </div>
   );
