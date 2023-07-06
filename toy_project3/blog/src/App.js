719dd3c8593ca43ca61af01116cd792f5fd1bf22
605s <ploki7335@naver.com>
2023-07-06 15:27:06 +0900

    Add NewProject

---
+++ toy_project3/blog/src/App.js
@@ -0,0 +1,13 @@
+import Post from "./components/Post";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Post author="hoya" body="Hello Everybody!" />
+      <Post author="gwangho" body="Hi Everyone!" />
+    </div>
+  );
+}
+
+export default App;
